0bac714e2225e7e38b4001c677a9d2994c210ee5
HunainHumail <37023744+HunainHumail@users.noreply.github.com>
2019-05-18 10:36:49 +0500

    added first notebook
    Basic python tasks

---
+++ Untitled.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("Hello Hunain")
+
+
+# In[2]:
+
+
+print("hey")
+
+
+# In[3]:
+
+
+a = 1
+b= 2
+c=4
+d= a+b+c
+print (d)
+
+
+# In[ ]:
+
+
+
+
---
+++ Untitled2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+age = 10
+name = "Asad"
+print("my name is "+name+" and my age is "+str(age))
+
+
+# In[8]:
+
+
+age = float(input("Please enter your age: "))
+age += 12
+name = input("Please enter your name: ")
+print("my name is "+name+" and my age is "+str(age))
+
+
+# In[10]:
+
+
+age = int(input("Please enter your age: "))
+age += 12
+name = input("Please enter your name: ")
+print("my name is {1} and my age is {0} ".format(name,age))
+
+
+# In[14]:
+
+
+a = 10
+b = 20
+if(a<b):
+    print("Chotta hai A")
+    print()
+    print()
+    print()
+print("I am out of body")
+
+
+# In[20]:
+
+
+
+
+
+# In[29]:
+
+
+number = input("Please enter any number: ")
+if (int(number)%2 != 0):
+    print("number is odd")
+else:
+    print("number is even")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
